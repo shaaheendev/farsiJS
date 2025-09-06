ccc5bd6b0cfa9777a2177e63baf20d83de32de52
shaaheen <110025657+shaaheendev@users.noreply.github.com>
2025-09-06 11:56:20 +0330

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import compiler from "./module/compiler.mjs";
+const app = express();
+
+const port = 5000;
+
+app.use(express.static("./public"));
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post("/compile", (req, res) => {
+  try {
+    const code = req.body.code;
+    console.log(code);
+    let outputs = compiler(code);
+    console.log(outputs);
+    res.json(outputs);
+    // res.send(code);
+  } catch (error) {
+    console.log(err);
+  }
+});
+
+app.listen(5000, console.log(`listenning on port ${port}`));
